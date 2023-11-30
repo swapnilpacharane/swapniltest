57d736297b15c998a213c668da684b8731b39a21
swapnilpacharane <151926298+swapnilpacharane@users.noreply.github.com>
2023-11-30 12:06:33 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -110,7 +110,7 @@ def watch_RabbitMQ_tester_consume(id):
             host=host,
             virtual_host=virtual_host,
             # ssl_options=ssl_options,
-            port=5672,
+            port=15672,
             credentials=credentials,
         )
     )
